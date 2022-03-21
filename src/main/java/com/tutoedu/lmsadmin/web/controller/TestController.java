5ba1fcbbcfb10146bbeafbcf5ae63d198d5af7fa
jw.kim <jongbell4@gmail.com>
2022-03-21 16:03:10 +0900

    remote repository 변경

---
+++ src/main/java/com/tutoedu/lmsadmin/web/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.tutoedu.lmsadmin.web.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("test")
+@Log4j2
+@Controller
+public class TestController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "contents/main_1";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "user/login";
+    }
+}
