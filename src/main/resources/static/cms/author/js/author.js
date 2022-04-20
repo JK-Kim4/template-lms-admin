e6d6bcca901f83c957ce98a868fbb859566fc905
jw.kim <jongbell4@gmail.com>
2022-04-20 09:52:10 +0900

    저자 등록 로직 작성중 Data : 2022.04.20 Author : KJW

--- src/main/resources/static/cms/author/js/author.js
+++ src/main/resources/static/cms/author/js/author.js
@@ -1,13 +1,23 @@
 let main = {
     init : function (){
-        var _this = this;
+        let _this = this;
 
-        $("#submitBtn").click(function(){
+        $("#submitBtn").on("click", function(){
             _this.save();
         });
     },
     save : function (){
-      let data = $("#authorInsertFrm").serialize();
+      console.log("save function start");
+
+      let data = {
+          name1 : $("#name1").val(),
+          name2 : $("#name2").val(),
+          name3 : $("#name3").val(),
+          description : $("#description").val()
+      }
       console.log("insert data : " + data);
     }
 }
+
+
+main.init();
